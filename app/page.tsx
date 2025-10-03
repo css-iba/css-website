e08df46fe42c93e00e10b822a3d416f18079ae85
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-10-03 19:22:51 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,35 +4,20 @@ import * as React from "react";
 import { HomeCarousel } from "@/components/Home/Carousel";
 import TitleSection from "@/components/Home/TitleSection";
 import DescriptionSection from "@/components/Home/DescriptionSection";
+// import Launch from "@/components/Home/Launch";
 import CodeX from "@/components/Home/CodeX";
 import ProBattle from "@/components/Home/ProBattle";
 import TeamSection from "@/components/Home/TeamSection";
 import FAQ  from "@/components/Home/FAQ";
-// import WelcomeScreen from "@/components/Home/WelcomeScreen";
 
 const Home: React.FC = () => {
-  // const [showWelcome, setShowWelcome] = React.useState(false);
-
-  // React.useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     const hasShown = sessionStorage.getItem("homeWelcomeShown");
-  //     if (!hasShown) {
-  //       setShowWelcome(true);
-  //       const timer = setTimeout(() => {
-  //         setShowWelcome(false);
-  //         sessionStorage.setItem("homeWelcomeShown", "true");
-  //       }, 2000);
-  //       return () => clearTimeout(timer);
-  //     }
-  //   }
-  // }, []);
 
   return (
     <div className="colour-bg min-h-screen">
-      {/* <WelcomeScreen show={showWelcome} onFinish={() => setShowWelcome(false)} /> */}
         <TitleSection />
         <HomeCarousel />
         <DescriptionSection />
+        {/* <Launch /> */}
         <CodeX />
         <ProBattle />
         <TeamSection />
