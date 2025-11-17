3f499d9832ac15cb181065c03e704d4c8187e479
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-11-17 22:15:27 +0500

    Add sponsors interface and data to Constants.ts

---
+++ app/ProBattle/Constants.ts
@@ -0,0 +1,59 @@
+interface Sponsor {
+    logoUrl: string;
+    size?: string;
+}
+
+export const sponsors: Sponsor[] = [
+    {
+        logoUrl: "/ProBattle/HBL.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/Daraz.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/Allied.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/Careem.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/Systems.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/Toyota.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/MLSA.png",
+        size: "large"
+    },
+    {
+        logoUrl: "/ProBattle/RoboAutomators.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/Home/notion-logo.png",
+        size: "large"
+    },
+    {
+        logoUrl: "/Home/DC_Donates_logo.png",
+        size: "large"
+    },
+    {
+        logoUrl: "/ProBattle/Kalsoft.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/SudoWare.png",
+        size: "small"
+    },
+    {
+        logoUrl: "/ProBattle/IBM.png",
+        size: "small"
+    }
+];
