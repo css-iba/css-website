ceeee86fbcde33d30524c04df08dfa5bf182ecd6
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-11-17 22:26:01 +0500

    Update page.tsx

--- app/ProBattle/page.tsx
+++ app/ProBattle/page.tsx
@@ -2,11 +2,13 @@
 
 import * as React from "react";
 import About from "@/components/ProBattle/About";
+import Sponsors from "@/components/ProBattle/Sponsors";
 
 const ProBattle: React.FC = () => {
   return (
     <main className="min-h-screen colour-bg">
       <About />
+      <Sponsors />
     </main>
   );
 };
