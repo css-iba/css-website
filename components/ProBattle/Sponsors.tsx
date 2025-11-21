9359a68d4144a77d76fb02dc26300c48aed0990a
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-11-21 21:51:56 +0500

    Increase sponsor header font size

--- components/ProBattle/Sponsors.tsx
+++ components/ProBattle/Sponsors.tsx
@@ -6,7 +6,7 @@ const Sponsors: React.FC = () => {
   return (
     <div className="colour-box-secondary py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-2xl font-semibold text-gray-900 font-heading">
+        <h2 className="text-center text-4xl font-semibold text-gray-900 font-heading">
           Our Previous Sponsors
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
