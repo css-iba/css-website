b33578d53d2808361ce1d8390df14f183ec430c2
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2025-12-28 18:59:53 +0500

    Add RegisterCTA component to ProBattle page

--- app/ProBattle/page.tsx
+++ app/ProBattle/page.tsx
@@ -5,12 +5,14 @@ import About from "@/components/ProBattle/About";
 import Sponsors from "@/components/ProBattle/Sponsors";
 import ModulesSection from "@/components/ProBattle/ModulesSection";
 import Contact from "@/components/ProBattle/Contact";
+import RegisterCTA from "@/components/ProBattle/RegisterCTA"
 
 const ProBattle: React.FC = () => {
   return (
     <main className="min-h-screen colour-bg">
       <About />
       <ModulesSection />
+      <RegisterCTA />
       <Contact />
       <Sponsors />
     </main>
