ba6e4945c49ece1537011907a3a19b6da2c25f01
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2026-01-13 18:31:49 +0500

    Update constants.ts

--- app/ProBattle/constants.ts
+++ app/ProBattle/constants.ts
@@ -175,7 +175,7 @@ const modulesData: ModuleCardProps[] = [
     minParticipants: 1,
     maxParticipants: 3,
     price: 1500,
-    guideLink: '',
+    guideLink: 'https://drive.google.com/file/d/1lBWwWJRy57faJDgDk3Qc_1wx8ckdKO_l/view?usp=sharing',
     category: 'Technical',
     Tier: [2]
   },
