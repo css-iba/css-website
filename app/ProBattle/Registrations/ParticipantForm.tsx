4ddb58fb6865ddcef8d01b9148f62661d894314c
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2026-01-22 19:49:24 +0500

    Update ParticipantForm.tsx

--- app/ProBattle/Registrations/ParticipantForm.tsx
+++ app/ProBattle/Registrations/ParticipantForm.tsx
@@ -340,21 +340,12 @@ export default function ParticipantForm() {
             </p>
 
             <Link
-              href="https://ibaapps.iba.edu.pk/society_events/register/event/probattle-26"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-block px-6 py-3 rounded-lg font-semibold transition colour-box-primary text-white hover:scale-[1.02] duration-200"
-            >
-              Go to Payment Portal →
-            </Link>
-
-            <Link
               href="https://ibaapps.iba.edu.pk/society_events/register/event/probattle-26-robotics"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block px-6 py-3 rounded-lg font-semibold transition colour-box-primary text-white hover:scale-[1.02] duration-200"
             >
-              For robotics payment →
+              Go to Payment Portal →
             </Link>
 
             {/* <p className="text-base text-gray-600 mb-4">
